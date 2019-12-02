a66eeee74c3cded038be20119b9dc85b32bdbcc2
Josep M Sobrepere <josepot@gmail.com>
2019-12-02 15:20:15 +0000

    Day 2 cleanup

--- src/2/solution.ts
+++ src/2/solution.ts
@@ -1,40 +1,45 @@
 export {};
 
+const operations: Record<number, (...args: number[]) => number> = {
+  1: (a: number, b: number) => a + b,
+  2: (a: number, b: number) => a * b
+};
+
+const EXIT_CODE = 99;
 const solution = (a: number, b: number, instructions: number[]) => {
   instructions[1] = a;
   instructions[2] = b;
-
   let currentIdx = 0;
-  while (instructions[currentIdx] !== 99) {
-    const instruction = instructions[currentIdx];
-    const input1Idx = instructions[currentIdx + 1];
-    const input2Idx = instructions[currentIdx + 2];
-    const outputIdx = instructions[currentIdx + 3];
 
-    let result: number;
-    if (instruction === 1) {
-      result = instructions[input2Idx] + instructions[input1Idx];
-    } else if (instruction === 2) {
-      result = instructions[input2Idx] * instructions[input1Idx];
-    } else {
-      throw new Error("Something went wrong " + instruction);
+  while (instructions[currentIdx] !== EXIT_CODE) {
+    const operationKey = instructions[currentIdx++];
+    const fn = operations[operationKey];
+    if (fn === undefined) {
+      throw new Error(`Invalid operation with code ${operationKey}`);
     }
-    instructions[outputIdx] = result;
-    currentIdx += 4;
+
+    const args = new Array(fn.length);
+    for (let i = 0; i < fn.length; i++) {
+      args[i] = instructions[instructions[currentIdx++]];
+    }
+    instructions[instructions[currentIdx++]] = fn(...args);
   }
 
   return instructions[0];
 };
+
 const solution1 = (lines: string[]) =>
   solution(12, 2, lines[0].split(",").map(Number));
 
 const solution2 = (lines: string[]) => {
-  const instructions = lines[0].split(",").map(Number);
+  const TARGET_SOLUTION = 19690720;
+  const originalInstructions = lines[0].split(",").map(Number);
   for (let a = 0; a < 100; a++) {
     for (let b = 0; b < 100; b++) {
-      const newInstructions = [...instructions];
-      if (solution(a, b, newInstructions) === 19690720)
-        return 100 * newInstructions[1] + newInstructions[2];
+      const instructions = [...originalInstructions];
+      if (solution(a, b, instructions) === TARGET_SOLUTION) {
+        return 100 * instructions[1] + instructions[2];
+      }
     }
   }
   return "Something bad happened";
