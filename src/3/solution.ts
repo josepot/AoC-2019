9786777a2ee3cbfc9484138bf9072b3eee85d25d
Josep M Sobrepere <josepot@gmail.com>
2019-12-03 06:39:27 +0000

    Day 3 cleanup

--- src/3/solution.ts
+++ src/3/solution.ts
@@ -2,81 +2,76 @@ import add from "utils/ts/add";
 
 export {};
 
-const solution1 = (lines: string[]) => {
-  const wires = lines.map(x =>
-    x.split(",").map(w => ({ direction: w[0], len: Number(w.slice(1)) }))
+type Direction = "U" | "D" | "L" | "R";
+const directionInstructions: Record<Direction, ["x" | "y", number]> = {
+  U: ["y", 1],
+  D: ["y", -1],
+  L: ["x", -1],
+  R: ["x", 1]
+};
+
+const getWires = (lines: string[]) =>
+  lines.map(x =>
+    x
+      .split(",")
+      .map(w => ({ direction: w[0] as Direction, len: Number(w.slice(1)) }))
   );
-  const grid = new Map<string, Map<number, number>>();
+
+const solution1 = (lines: string[]) => {
+  const grid = new Map<string, Set<number>>();
+  const wires = getWires(lines);
+
   wires.forEach((wire, idx) => {
-    let x = 0;
-    let y = 0;
-    let steps = 0;
+    const currentPosition = { x: 0, y: 0 };
     wire.forEach(({ direction, len }) => {
-      if (direction === "U") {
-        for (let test = 0; test < len; test++) {
-          steps++;
-          y += 1;
-          const key = [x, y].join(",");
+      const [prop, delta] = directionInstructions[direction];
+      for (let t = 0; t < len; t++) {
+        currentPosition[prop] += delta;
+        const key = [currentPosition.x, currentPosition.y].join(",");
 
-          if (!grid.has(key)) {
-            grid.set(key, new Map([[idx, steps]]));
-          } else if (!grid.get(key)!.has(idx)) {
-            grid.get(key)!.set(idx, steps);
-          }
-        }
-      } else if (direction === "D") {
-        for (let test = 0; test < len; test++) {
-          steps++;
-          y -= 1;
-          const key = [x, y].join(",");
-
-          if (!grid.has(key)) {
-            grid.set(key, new Map([[idx, steps]]));
-          } else if (!grid.get(key)!.has(idx)) {
-            grid.get(key)!.set(idx, steps);
-          }
-        }
-      } else if (direction === "R") {
-        for (let test = 0; test < len; test++) {
-          x += 1;
-          steps++;
-          const key = [x, y].join(",");
-          if (!grid.has(key)) {
-            grid.set(key, new Map([[idx, steps]]));
-          } else if (!grid.get(key)!.has(idx)) {
-            grid.get(key)!.set(idx, steps);
-          }
-        }
-      } else if (direction === "L") {
-        for (let test = 0; test < len; test++) {
-          x -= 1;
-          steps++;
-          const key = [x, y].join(",");
-
-          if (!grid.has(key)) {
-            grid.set(key, new Map([[idx, steps]]));
-          } else if (!grid.get(key)!.has(idx)) {
-            grid.get(key)!.set(idx, steps);
-          }
+        if (!grid.has(key)) {
+          grid.set(key, new Set([idx]));
+        } else if (!grid.get(key)!.has(idx)) {
+          grid.get(key)!.add(idx);
         }
       }
     });
   });
-  /*
-  SOLUTION 1
+
   return [...grid.keys()]
-    .map(key => ({
-      distance: key
+    .filter(key => grid.get(key)!.size === 2)
+    .map(key =>
+      key
         .split(",")
         .map(Number)
         .map(x => Math.abs(x))
-        .reduce(add),
-      points: grid.get(key)?.size,
-      key
-    }))
-    .filter(x => x.points === 2)
-    .sort((a, b) => a.distance - b.distance)[0].distance;
-   */
+        .reduce(add)
+    )
+    .sort((a, b) => a - b)[0];
+};
+
+const solution2 = (lines: string[]) => {
+  const grid = new Map<string, Map<number, number>>();
+  const wires = getWires(lines);
+
+  wires.forEach((wire, idx) => {
+    const currentPosition = { x: 0, y: 0 };
+    let steps = 0;
+    wire.forEach(({ direction, len }) => {
+      const [prop, delta] = directionInstructions[direction];
+      for (let t = 0; t < len; t++) {
+        steps++;
+        currentPosition[prop] += delta;
+        const key = [currentPosition.x, currentPosition.y].join(",");
+
+        if (!grid.has(key)) {
+          grid.set(key, new Map([[idx, steps]]));
+        } else if (!grid.get(key)!.has(idx)) {
+          grid.get(key)!.set(idx, steps);
+        }
+      }
+    });
+  });
 
   return [...grid.keys()]
     .filter(key => grid.get(key)!.size === 2)
@@ -84,4 +79,4 @@ const solution1 = (lines: string[]) => {
     .sort((a, b) => a - b)[0];
 };
 
-module.exports = [solution1, solution1];
+module.exports = [solution1, solution2];
