740e8a057d52108b1c961cf367d0478d03dde126
Josep M Sobrepere <josepot@gmail.com>
2019-12-04 07:08:38 +0000

    Day 4 cleanup

--- src/4/solution.ts
+++ src/4/solution.ts
@@ -1,78 +1,45 @@
 export {};
 
-const FROM = 136818;
-const TO = 685979;
-const checkIsValid = (x: number) => {
-  const arr = x
-    .toString()
-    .split("")
-    .map(Number);
+const checkIsValid1 = (arr: number[]) => {
+  let twoAdjacent = false;
+  let alwaysDecrease = true;
 
+  arr.forEach((current, idx) => {
+    twoAdjacent = twoAdjacent || current === arr[idx - 1];
+    alwaysDecrease = alwaysDecrease && (idx === 0 || arr[idx] >= arr[idx - 1]);
+  });
+
+  return alwaysDecrease && twoAdjacent;
+};
+
+const checkIsValid2 = (arr: number[]) => {
   let twoAdjacent = false;
   let alwaysDecrease = true;
 
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i - 1] === arr[i]) {
-      twoAdjacent = true;
-    }
+    twoAdjacent =
+      twoAdjacent ||
+      (arr[i - 1] === arr[i] && arr[i] !== arr[i + 1] && arr[i] !== arr[i - 2]);
     alwaysDecrease = alwaysDecrease && (i === 0 || arr[i] >= arr[i - 1]);
   }
 
   return alwaysDecrease && twoAdjacent;
 };
 
-const checkIsValid2 = (x: number) => {
-  const arr = x
-    .toString()
-    .split("")
-    .map(Number);
-
-  let alwaysDecrease = true;
-
-  const groups = new Map<number, number[]>();
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === arr[i - 1]) {
-      if (groups.has(arr[i])) {
-        const currArr = groups.get(arr[i])!;
-
-        if (arr[i] === arr[i - 2]) {
-          currArr[currArr.length - 1]++;
-        } else {
-          currArr.push(2);
-        }
-      } else {
-        groups.set(arr[i], [2]);
-      }
-    }
-
-    alwaysDecrease = alwaysDecrease && (i === 0 || arr[i] >= arr[i - 1]);
-  }
-
-  if (!alwaysDecrease) return false;
-  if (groups.size === 0) return false;
-
-  return [...groups.values()].some(x => x.every(y => y === 2));
-};
-
-const solution1 = () => {
+const solution = (checkFn: (x: number[]) => boolean) => ([input]: string[]) => {
+  const [FROM, TO] = input.split("-").map(Number);
   let nValid = 0;
   for (let i = FROM; i < TO; i++) {
-    if (checkIsValid(i)) {
+    const digits = i
+      .toString(10)
+      .split("")
+      .map(Number);
+
+    if (checkFn(digits)) {
       nValid++;
     }
   }
   return nValid;
 };
 
-const solution2 = () => {
-  let nValid = 0;
-  for (let i = FROM; i < TO; i++) {
-    if (checkIsValid2(i)) {
-      nValid++;
-    }
-  }
-  return nValid;
-};
-
-module.exports = [solution1, solution2];
+module.exports = [checkIsValid1, checkIsValid2].map(solution);
