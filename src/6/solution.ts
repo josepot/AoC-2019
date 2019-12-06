912694415c215fdcfc6f1014ec98722121d0fd3b
Josep M Sobrepere <josepot@gmail.com>
2019-12-06 08:59:07 +0100

    Day 6 final cleanup?

--- src/6/solution.ts
+++ src/6/solution.ts
@@ -1,22 +1,14 @@
 import graphDistinctSearch from "../utils/ts/graphDistinctSearch";
-export {};
-
-const getMaps = (lines: string[]) => {
-  const map = new Map<string, Set<string>>();
-  const reverseMap = new Map<string, string>();
-  lines
-    .map(line => line.split(")"))
-    .forEach(([a, b]) => {
-      map.get(a)?.add(b) || map.set(a, new Set([b]));
-      reverseMap.set(b, a);
-    });
-  return [map, reverseMap] as const;
-};
-
-const solution1 = (
+interface Node {
+  id: string;
+  steps: number;
+}
+type SolutionFn = (
   map: Map<string, Set<string>>,
   reverseMap: Map<string, string>
-) => {
+) => number;
+
+const solution1: SolutionFn = (map, reverseMap) => {
   const orbits = new Map<string, number>();
   const getOrbits = (key: string): number => {
     if (orbits.has(key)) return orbits.get(key)!;
@@ -30,15 +22,7 @@ const solution1 = (
   return [...orbits.values()].reduce((a, b) => a + b);
 };
 
-interface Node {
-  id: string;
-  steps: number;
-}
-
-const solution2 = (
-  map: Map<string, Set<string>>,
-  reverseMap: Map<string, string>
-) =>
+const solution2: SolutionFn = (map, reverseMap) =>
   graphDistinctSearch(
     { id: "YOU", steps: 0 },
     (node: Node) =>
@@ -52,6 +36,14 @@ const solution2 = (
     (a: Node, b: Node) => b.steps - a.steps
   ).steps - 2;
 
-module.exports = [solution1, solution2].map(fn => (lines: string[]) =>
-  fn(...getMaps(lines))
-);
+export default [solution1, solution2].map(fn => (lines: string[]) => {
+  const map = new Map<string, Set<string>>();
+  const reverseMap = new Map<string, string>();
+  lines
+    .map(line => line.split(")"))
+    .forEach(([a, b]) => {
+      map.get(a)?.add(b) || map.set(a, new Set([b]));
+      reverseMap.set(b, a);
+    });
+  return fn(map, reverseMap);
+});
