9f54cd8b4c4ae1c03ad21cc92ddf311ad33d92f7
Josep M Sobrepere <josepot@gmail.com>
2019-12-08 16:14:10 +0100

    Day 8 final cleanup

--- src/8/solution.ts
+++ src/8/solution.ts
@@ -1,58 +1,40 @@
-const wide = 25;
-const tall = 6;
-const digitsInLayer = 25 * 6;
+import nestData from "utils/ts/nestData";
+
+const WIDTH = 25;
+const HEIGHT = 6;
 enum Color {
   W = "#",
   B = " ",
   T = "."
 }
-
 const colors = [Color.B, Color.W, Color.T];
 
-const solution1 = (layers: number[][][]) => {
-  const winner = layers
+const solution1 = (layers: Color[][][]) =>
+  layers
     .map(layer =>
-      layer
-        .reduce((a, b) => [...a, ...b], [] as number[])
-        .reduce((a, b) => {
-          const current = a.get(b) || 0;
-          a.set(b, current + 1);
-          return a;
-        }, new Map<number, number>())
+      layer.flat(1).reduce((a, b) => {
+        a.set(b, a.get(b)! + 1);
+        return a;
+      }, new Map<Color, number>(colors.map(color => [color, 0])))
     )
-    .sort((a, b) => (a.get(0) || 0) - (b.get(0) || 0))[0];
+    .sort((a, b) => a.get(Color.B)! - b.get(Color.B)!)
+    .filter((_, idx) => idx === 0)
+    .map(winner => winner.get(Color.T)! * winner.get(Color.W)!)[0];
 
-  return winner.get(1)! * winner.get(2)!;
-};
+const getPixelColor = (y: number, x: number, layers: Color[][][]): Color =>
+  layers.map((_, idx) => layers[idx][y][x]).find(x => x !== Color.T) || Color.T;
 
-const solution2 = (rawLayers: number[][][]) => {
-  const layers = rawLayers.map(layer =>
-    layer.map(row => row.map(digit => colors[digit]))
+const solution2 = (layers: Color[][][]) =>
+  layers[0].forEach((row, y) =>
+    console.log(row.map((_, x) => getPixelColor(y, x, layers)).join(""))
   );
 
-  const getPixelColor = (y: number, x: number): Color =>
-    layers
-      .map((_, idx) => layers[idx][y][x])
-      .find(x => x === Color.B || x === Color.W) || Color.T;
-
-  layers[0].forEach((row, y) => {
-    console.log(row.map((_, x) => getPixelColor(y, x)).join(""));
-  });
-};
-
-export default [solution1, solution2].map(fn => ([line]: string[]) => {
-  const nLayers = line.length / digitsInLayer;
-  const layers: number[][][] = [];
-  for (let l = 0; l < nLayers; l++) {
-    const layer: number[][] = [];
-    for (let y = 0; y < tall; y++) {
-      let pixel: number[] = [];
-      for (let x = 0; x < wide; x++) {
-        pixel.push(Number(line[l * digitsInLayer + (y * wide + x)]));
-      }
-      layer.push(pixel);
-    }
-    layers.push(layer);
-  }
-  return fn(layers);
-});
+export default [solution1, solution2].map(fn => ([line]: string[]) =>
+  fn(
+    nestData(
+      line.split("").map(x => colors[Number(x)]),
+      WIDTH,
+      HEIGHT
+    )
+  )
+);
