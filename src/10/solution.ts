92eed2b27cdb79c31197f7e454e44b2b7e86cce0
Josep M Sobrepere <josepot@gmail.com>
2019-12-10 18:29:49 +0100

    Day 10 first cleanup

--- src/10/solution.ts
+++ src/10/solution.ts
@@ -1,5 +1,5 @@
 const map = new Map<string, boolean>();
-let SIZE = 0;
+let SIZE: number;
 
 const getAsteroidsInSight = (x: number, y: number) => {
   const blockedAngles: [number, number][] = [];
@@ -43,10 +43,9 @@ const getAsteroidsInSight = (x: number, y: number) => {
   return detected;
 };
 
-const solution1 = (lines: string[]) => {
-  SIZE = lines.length;
+const getBaseAsteroidWithAsteroidsInSight = (lines: string[]) => {
   const positions: [number, number][] = [];
-
+  SIZE = lines.length;
   lines.forEach((line, y) =>
     line.split("").forEach((val, x) => {
       positions.push([x, y]);
@@ -56,37 +55,41 @@ const solution1 = (lines: string[]) => {
 
   return positions
     .filter(([x, y]) => map.get([x, y].join(",")))
-    .map(([x, y]) => getAsteroidsInSight(x, y))
-    .sort((a, b) => b.size - a.size)[0].size;
+    .map(([x, y]) => [getAsteroidsInSight(x, y), x, y] as const)
+    .sort((a, b) => b[0].size - a[0].size)[0];
 };
 
-const getClockwiseAngle = ([x, y]: [number, number]): number => {
-  const result = Math.atan2(x, y);
-  return result < 0 ? Math.PI * 2 + result : result;
-};
+const solution1 = (asteroidsInSight: Set<string>) => asteroidsInSight.size;
 
-const solution2 = (lines: string[]) => {
-  SIZE = lines.length;
-  const positions: [number, number][] = [];
+const solution2 = (
+  asteroidsInSight: Set<string>,
+  baseX: number,
+  baseY: number
+) => {
+  const N_WINNER = 200;
+  const getClockwiseAngle = ([x, y]: [number, number]): number => {
+    const result = Math.atan2(x, y);
+    return result < 0 ? Math.PI * 2 + result : result;
+  };
 
-  lines.forEach((line, y) =>
-    line.split("").forEach((val, x) => {
-      positions.push([x, y]);
-      map.set([x, y].join(","), val === "#");
-    })
-  );
+  let currentRound = [...asteroidsInSight.values()];
+  let totalDestroyedAsteroids = currentRound.length;
+  while (totalDestroyedAsteroids < N_WINNER) {
+    currentRound.forEach(key => map.delete(key));
+    currentRound = [...getAsteroidsInSight(baseX, baseY).values()];
+    totalDestroyedAsteroids += currentRound.length;
+  }
 
-  const [baseX, baseY, firstRound] = positions
-    .filter(([x, y]) => map.get([x, y].join(",")))
-    .map(([x, y]) => [x, y, getAsteroidsInSight(x, y)] as const)
-    .sort((a, b) => b[2].size - a[2].size)[0];
-
-  const [winnerX, winnerY] = [...firstRound.values()]
+  const winnerIdx =
+    N_WINNER - (totalDestroyedAsteroids - currentRound.length) - 1;
+  const [winnerX, winnerY] = currentRound
     .map(key => key.split(",").map(Number) as [number, number])
     .map(([x, y]) => [x - baseX, baseY - y] as [number, number])
-    .sort((a, b) => getClockwiseAngle(a) - getClockwiseAngle(b))[199];
+    .sort((a, b) => getClockwiseAngle(a) - getClockwiseAngle(b))[winnerIdx];
 
   return (winnerX + baseX) * 100 + (baseY - winnerY);
 };
 
-export default [solution1, solution2];
+export default [solution1, solution2].map(fn => (lines: string[]) =>
+  fn(...getBaseAsteroidWithAsteroidsInSight(lines))
+);
