0a25e228a24196fab529dc670b41813e50a759c2
Josep M Sobrepere <josepot@gmail.com>
2019-12-10 15:53:08 +0100

    Day 10 part 2 raw

--- src/10/solution.ts
+++ src/10/solution.ts
@@ -1,7 +1,7 @@
 const map = new Map<string, boolean>();
 let SIZE = 0;
 
-const getAsteroidsInSight = (x: number, y: number): number => {
+const getAsteroidsInSight = (x: number, y: number) => {
   const blockedAngles: [number, number][] = [];
   const detected = new Set<string>();
 
@@ -40,7 +40,7 @@ const getAsteroidsInSight = (x: number, y: number): number => {
       detected.add(targetKey);
     });
   }
-  return detected.size;
+  return detected;
 };
 
 const solution1 = (lines: string[]) => {
@@ -57,7 +57,36 @@ const solution1 = (lines: string[]) => {
   return positions
     .filter(([x, y]) => map.get([x, y].join(",")))
     .map(([x, y]) => getAsteroidsInSight(x, y))
-    .sort((a, b) => b - a)[0];
+    .sort((a, b) => b.size - a.size)[0].size;
 };
 
-export default [solution1];
+const getClockwiseAngle = ([x, y]: [number, number]): number => {
+  const result = Math.atan2(x, y);
+  return result < 0 ? Math.PI * 2 + result : result;
+};
+
+const solution2 = (lines: string[]) => {
+  SIZE = lines.length;
+  const positions: [number, number][] = [];
+
+  lines.forEach((line, y) =>
+    line.split("").forEach((val, x) => {
+      positions.push([x, y]);
+      map.set([x, y].join(","), val === "#");
+    })
+  );
+
+  const [baseX, baseY, firstRound] = positions
+    .filter(([x, y]) => map.get([x, y].join(",")))
+    .map(([x, y]) => [x, y, getAsteroidsInSight(x, y)] as const)
+    .sort((a, b) => b[2].size - a[2].size)[0];
+
+  const [winnerX, winnerY] = [...firstRound.values()]
+    .map(key => key.split(",").map(Number) as [number, number])
+    .map(([x, y]) => [x - baseX, baseY - y] as [number, number])
+    .sort((a, b) => getClockwiseAngle(a) - getClockwiseAngle(b))[199];
+
+  return (winnerX + baseX) * 100 + (baseY - winnerY);
+};
+
+export default [solution1, solution2];
