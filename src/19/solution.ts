268b06b4d16aca174e22516dcef99c6c4e202548
Josep M Sobrepere <josepot@gmail.com>
2019-12-19 08:11:21 +0100

    Day 19 cleanup

--- src/19/solution.ts
+++ src/19/solution.ts
@@ -1,16 +1,32 @@
 import { intCodeProcessor } from "utils/ts/intCodeGenerator";
-import {
-  movePosition,
-  Position,
-  getAdjacentPositions,
-  Direction,
-  getDirectionWheel,
-  turnWheel,
-  getPositionFromKey
-} from "utils/ts/directions";
-import printPositionsMap from "utils/ts/printPositionsMap";
 
 const solution1 = async ([line]: string[]) => {
+  const visitedPositions = new Map<string, number>();
+  const inputs = Array(50)
+    .fill(null)
+    .map((_, y) =>
+      Array(50)
+        .fill(null)
+        .map((_, x) => [x, y])
+    )
+    .flat();
+
+  await Promise.all(
+    inputs.map(async input => {
+      let i = 0;
+      const result = await intCodeProcessor(
+        line,
+        x => x,
+        () => input[i++]
+      );
+      visitedPositions.set(input.join(","), result);
+    })
+  );
+
+  return [...visitedPositions.values()].filter(x => x === 1).length;
+};
+
+const solution2 = async ([line]: string[]) => {
   const getValueAtPosition = (x: number, y: number) => {
     const vals = [x, y];
     let i = 0;
@@ -20,18 +36,6 @@ const solution1 = async ([line]: string[]) => {
       () => vals[i++]
     );
   };
-
-  const visitedPositions = new Map<string, number>();
-  const inputs = Array(200)
-    .fill(null)
-    .map((_, y) =>
-      Array(200)
-        .fill(null)
-        .map((_, x) => [x + 105 * 10, y + 70 * 10])
-    )
-    .flat();
-  let i = 0;
-
   const getLastBeamAtLine = async (y: number, previousX: number) => {
     let current: number;
     let next: number;
@@ -52,73 +56,14 @@ const solution1 = async ([line]: string[]) => {
     previousX = await getLastBeamAtLine(previousY, previousX);
     const xx = await getValueAtPosition(previousX - 99, previousY);
     if (xx === 0) {
-      console.log("not long enough", previousX, previousY);
       continue;
     }
     const yy = await getValueAtPosition(previousX - 99, previousY + 99);
     if (yy === 0) {
-      console.log("not tall enough", previousX, previousY);
       continue;
     }
     return (previousX - 99) * 10000 + previousY;
   } while (true);
-
-  /*
-  await Promise.all(
-    inputs.map(input => {
-      let i = 0;
-      return intCodeProcessor(
-        line,
-        x => x,
-        () => input[i++]
-      ).then(result => {
-        visitedPositions.set(input.join(","), result);
-      });
-    })
-  );
-
-  const lastBeamsForLine = Array(90)
-    .fill(null)
-    .map((_, y) => {
-      let result: Position;
-      for (let x = 0; x < 120; x++) {
-        const current = [x, y].join(",");
-        const next = [x + 1, y].join(",");
-        if (
-          visitedPositions.get(next) === 0 &&
-          visitedPositions.get(current) === 1
-        ) {
-          return getPositionFromKey(current);
-        }
-      }
-    });
-
-  const findWhereNFit = (n: number) => {
-    for (let y = 0; y < 90; y++) {
-      const last = lastBeamsForLine[y];
-      if (last === undefined) {
-        continue;
-      }
-      if (
-        visitedPositions.get([last.x - n, last.y].join(",")) === 1 &&
-        visitedPositions.get([last.x - n, last.y + n].join(",")) === 1
-      ) {
-        return [last.x - n, last.y];
-      }
-    }
-  };
-  console.log(printPositionsMap(visitedPositions, [".", "#"]));
-   */
-  /*
-
-  for (let i = 2; i < 9; i++) {
-    console.log(i, findWhereNFit(i));
-  }
-   */
 };
 
-const solution2 = async ([line]: string[]) => {
-  return line;
-};
-
-export default [solution1];
+export default [solution1, solution2];
