4e94f206ea357c27a901d105109d27982d98e258
Josep M Sobrepere <josepot@gmail.com>
2019-12-21 10:39:32 +0100

    Day 21

---
+++ src/21/solution.ts
@@ -0,0 +1,76 @@
+import { intCodeProcessor } from "utils/ts/intCodeGenerator";
+import printPositionsMap from "utils/ts/printPositionsMap";
+
+const positions = new Map<string, number>();
+let currentX = 0;
+let currentY = 0;
+const buildGrid = (c: number) => {
+  if (c === 10) {
+    currentX = 0;
+    currentY++;
+    return;
+  }
+  positions.set(currentX + "," + currentY, c);
+  currentX++;
+};
+
+const aInstructions = `
+NOT A J
+NOT B T
+OR T J
+NOT C T
+OR T J
+AND D J
+WALK
+`;
+
+const bInstructions = `
+NOT A J
+NOT B T
+OR T J
+NOT C T
+OR T J
+AND D J
+NOT E T
+NOT T T
+OR H T
+AND T J
+RUN
+`;
+
+/*
+WRONG!!!
+previous && !((!e || !f || !g) && !h)
+
+previous && !(!e || !f || !g) || h
+
+previous && e && f && g || h
+
+CORRECT!
+previous && (E || H)
+*/
+
+export default [aInstructions, bInstructions].map(
+  instructions => ([line]: string[]) => {
+    const result = intCodeProcessor(
+      line,
+      buildGrid,
+      parseInstructions(instructions)
+    );
+    if (result < 1000) {
+      console.log(printPositionsMap(positions, x => String.fromCharCode(x)));
+      return;
+    }
+    return result;
+  }
+);
+
+const parseInstructions = (instructions: string) =>
+  instructions
+    .split("\n")
+    .slice(1)
+    .slice(0, -1)
+    .join("\n")
+    .split("")
+    .map(x => (x === "\n" ? 10 : x.charCodeAt(0)))
+    .concat(10);
