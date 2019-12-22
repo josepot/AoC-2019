a37573ad8122e0c587a374058c0169badf3101fd
Josep M Sobrepere <josepot@gmail.com>
2019-12-22 22:18:48 +0100

    Day 22 part 2 done

--- src/22/solution.ts
+++ src/22/solution.ts
@@ -53,47 +53,138 @@ const solution1 = (lines: string[]) => {
 
 const solution2 = (lines: string[]) => {
   const nCards: BigInteger = bigInt(119315717514047);
+  // const nCards: BigInteger = bigInt(10007);
   const iterations: BigInteger = bigInt(101741582076661);
+  /*
+  initialFirstCardVal = 0
+  initialInc = 1;
 
-  const newStack = (increment: BigInteger, offset: BigInteger) => {
-    const newIncrement = increment.multiply(-1);
-    return [newIncrement, offset.add(newIncrement).mod(nCards)];
-  };
+  getRelativeCardValue(idx, firstCardVal, increment) = (firstCardVal + (increment * idx)) % nCards
+  getCardValue(idx, firstCardVal, increment) = (getRelativeCardValue(idx, firstCardVal, increment) + nCards) % nCards
+   */
 
-  const cut = (increment: BigInteger, offset: BigInteger, n: BigInteger) =>
-    [increment, offset.add(increment.multiply(n)).mod(nCards)] as const;
+  /*
+  newStack:
+    nextIncrement = -currentIncrement
+    nextFirstCardVal = currentFirstCardVal - currentIncrement;
 
-  const dealInc = (increment: BigInteger, offset: BigInteger, n: BigInteger) =>
-    [increment.multiply(n.modInv(nCards)).mod(nCards), offset] as const;
+  Example:
+    - inc = 7
+    - firstCardVal = 4
+      => 4 1 8 5 2 9 6 3 0 7
+    newStack()
+    - nextIncrement = -7
+    - nextFirstCardVal = -3
+      => -3 0 -7 -4 -1 -8 -5 -2 -9 -6
+  */
+  const newStack = (increment: BigInteger, firstCardValue: BigInteger) => [
+    increment.multiply(-1),
+    firstCardValue.minus(increment).mod(nCards)
+  ];
 
-  let offset = bigInt(0);
+  /*
+  cut(n):
+    nextIncrement = currentIncrement
+    nextFirstCardVal = (currentFirstCardVal + (currentIncrement * n)) % nCards
+
+  Example:
+    - inc = 7
+    - firstCardVal = 8
+      => 8 5 2 9 6 3 0 7 4 1
+    cut(3)
+    - nextIncrement = 7
+    - nextFirstCardVal = 29 % 10 = 9
+      => 9 6 3 0 7 4 1 8 5 2
+    cut(-4)
+    - nextIncrement = 7
+    - nextFirstCardVal = -9
+      => -9 -2 5 2 9 6 3 0 7 4
+   */
+  const cut = (
+    increment: BigInteger,
+    firstCardValue: BigInteger,
+    n: BigInteger
+  ) =>
+    [increment, firstCardValue.add(increment.multiply(n)).mod(nCards)] as const;
+
+  /*
+  dealInc(n):
+  - nextFirstCardVal = currentFirstCArdVal
+  - nextIncrement = secondCardVal - firstCardVal
+      (currentIdx * n) % nCards = nextIdx
+        (currentIdxWhichWillBecomeIdx1 * n) % nCards = 1
+        currentIdxWhichWillBecomeIdx1 = modInv(n , nCards)
+      secondCardVal = getRelativeCardValue(modInv(n , nCards), currentFirstCardVal, currentIncrement)
+    nextIncrement = getRelativeCardValue(modInv(n , nCards), currentFirstCardVal, currentIncrement) - currentFirstCardVal
+    nextIncrement = currentFirstCardVal + (currentIncrement * modInv(n , nCards))) % nCards - currentFirstCardVal
+  **************************************************************************
+  ** - nextIncrement = (currentIncrement * modInv(n , nCards))) % nCards ***
+  **************************************************************************
+
+  Example:
+    - firstCardVal = 8
+    - inc = 7
+      => 8 5 2 9 6 3 0 7 4 1
+    dealInc(3)
+    - nextFirstCardVal = 8
+    - nextIncrement = (7 * 7) % 10 = 9
+      => 8 7 6 5 4 3 2 1 0 9
+    dealInc(9)
+    - nextFirstCardVal = 8
+    - nextIncrement = (9 * 9) % 10 = 1
+      => 8 9 0 1 2 3 4 5 6 7
+  */
+  const dealInc = (
+    increment: BigInteger,
+    firstCardValue: BigInteger,
+    n: BigInteger
+  ) =>
+    [increment.multiply(n.modInv(nCards)).mod(nCards), firstCardValue] as const;
+
+  let firstCardValue = bigInt(0);
   let inc = bigInt(1);
 
-  // for (let i = 0; i < 10; i++) {
+  // for (let i = 0; i < 20; i++) {
   lines.forEach(line => {
     if (line.startsWith("deal with increment ")) {
       const n = bigInt(Number(line.split(" ").slice(-1)[0]));
-      [inc, offset] = dealInc(inc, offset, n);
+      [inc, firstCardValue] = dealInc(inc, firstCardValue, n);
     } else if (line.startsWith("cut")) {
       const n = bigInt(Number(line.split(" ").slice(-1)[0]));
-      [inc, offset] = cut(inc, offset, n);
+      [inc, firstCardValue] = cut(inc, firstCardValue, n);
     } else {
-      [inc, offset] = newStack(inc, offset);
+      [inc, firstCardValue] = newStack(inc, firstCardValue);
     }
   });
-  console.log(inc.toJSNumber());
+  // console.log(inc.toJSNumber(), firstCardValue.toJSNumber());
   // }
+  // return;
+  /*
+  let iii = bigInt(inc);
+  let fff = bigInt(firstCardValue);
 
-  let t = bigInt(1);
-  for (let i = 0; i < 10; i++) {
-    t = t.multiply(inc).mod(nCards);
-    console.log(t.toJSNumber());
+  for (let i = 0; i < 20; i++) {
+    console.log(iii.toJSNumber(), fff.toJSNumber());
+    fff = fff.add(iii.multiply(firstCardValue)).mod(nCards);
+    iii = iii.multiply(inc).mod(nCards);
   }
+   */
 
-  return offset
-    .add(bigInt(2020).multiply(inc))
+  const finalInc = inc.modPow(iterations, nCards);
+  const finalFirstCard = firstCardValue
+    .multiply(
+      bigInt(1)
+        .minus(inc)
+        .mod(nCards)
+        .modInv(nCards)
+    )
+    .multiply(bigInt(1).minus(finalInc));
+
+  return finalFirstCard
+    .add(bigInt(2020).multiply(finalInc))
     .mod(nCards)
     .add(nCards)
+    .mod(nCards)
     .toJSNumber();
 };
 
